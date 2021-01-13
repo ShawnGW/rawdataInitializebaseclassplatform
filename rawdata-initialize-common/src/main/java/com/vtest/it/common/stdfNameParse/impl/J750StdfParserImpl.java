74c98c4816780645ee51f7fb9b846a77c2aff1d0
shawn <Mailsunguowei@163.com>
2021-01-13 20:44:29 +0800

    beta 1.4.2 perfect some issue and fix some bug

--- rawdata-initialize-common/src/main/java/com/vtest/it/common/stdfNameParse/impl/J750StdfParserImpl.java
+++ rawdata-initialize-common/src/main/java/com/vtest/it/common/stdfNameParse/impl/J750StdfParserImpl.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 
 public class J750StdfParserImpl implements StdfParser  {
 
-    private static final String regex = "VTEST_FT_(\\w{3})_([\\.\\w-]{1,}|[\\.\\w-_\\(\\)]{1,})_([A-Z0-9-\\.]{1,})_([\\w-\\.]{1,})_(TT[A-Z]{1}-[\\dA-Z]{2,4})_([0-9A-Z-]{1,})_([\\w-_]{1,})_(F[\\w0-9]{1,})_(RP[0-9]{1})_([Vv]{1}[0-9]{3,}|[0-9]{4,})_([0-9]{8}_[0-9]{6})\\.([A-Za-z0-9]{1,})";
+    private static final String regex = "VTEST_FT_(\\w{3})_([\\.\\w-]{1,}|[\\.\\w-_\\(\\)]{1,})_([A-Z0-9-\\.]{1,})_([\\w-\\.\\d]{1,})_(TT[A-Z]{1}-[\\dA-Z]{2,4}||[\\w\\d-]{1,})_([0-9A-Za-z-]{1,})_([\\w-_]{1,})_(F[\\w0-9]{1,})_(RP[0-9]{1})_([Vv]{1}[0-9]{3,}|[0-9]{4,})_([0-9]{8}_[0-9]{6})\\.([A-Za-z0-9]{1,})";
 
     public StdfInformationBean parse(File file) {
         Pattern pattern = Pattern.compile(regex);
